0bdc7fe78d161e9765c3329bae4d7e39238b63f1
blashbrook <blashbrook@users.noreply.github.com>
2025-07-30 12:44:37 -0500

    BREAKING CHANGES: add patron authentication

--- src/Clients/PAPIClient.php
+++ src/Clients/PAPIClient.php
@@ -3,6 +3,7 @@
 namespace Blashbrook\PAPIClient\Clients;
 
 use Blashbrook\PAPIClient\Concerns\Config;
+use Blashbrook\PAPIClient\Concerns\Formatters;
 use Blashbrook\PAPIClient\Concerns\Headers;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
@@ -13,15 +14,16 @@ use Psr\Http\Message\ResponseInterface;
  */
 class PAPIClient extends Client
 {
-    use Headers, Config;
+    use Headers, Config, Formatters;
 
     /**
      * Sends public request to Polaris API.  Public requests do not
      * require staff usernames and passwords.
      *
      * @param  $method  - HTTP Request method (GET|POST|PUT)
-     * @param  $requestURI
+     * @param  $requestURI - function-specific part of API Endpoint
      * @param  null[]  $params  - Optional request parameters
+     *
      * @return ResponseInterface
      *
      * @throws GuzzleException
@@ -38,4 +40,42 @@ class PAPIClient extends Client
                 'json' => $json, ],
         );
     }
+
+    /**
+     * Sends an authenticated patron's request to Polaris API.  Public patron requests
+     * require the AccessSecret returned from a successful patron authentication
+     *
+     * @param  $method  - HTTP Request method (GET|POST|PUT)
+     * @param  $requestURI - function-specific part of API Endpoint
+     * @param $accessSecret - AccessSecret returned after patron authentication
+     * @param  null[]  $params  - Optional request parameters
+     *
+     * @return ResponseInterface
+     *
+     * @throws GuzzleException
+     */
+    public static function authenticatedPatronRequest($method, $requestURI, $accessSecret, array $params = [null])
+    {
+        $uri = config('papiclient.publicURI').$requestURI;
+        $headers = self::getAuthenticatedPatronHeaders($method, $uri, $accessSecret);
+        $client = new Client();
+        $json = self::getPolarisSettings($params);
+
+        return $client->request($method, $uri,
+            ['headers' => $headers,
+                'json' => $json, ],
+        );
+    }
+
+    /**
+     * Formats timestamp in milliseconds to YYYY-MM-DD
+     *
+     * @param $timestamp
+     * @return string
+     */
+/*    public static function formatDate($timestamp): string
+    {
+        return Carbon::createFromTimestampMs($timestamp)->format('Y-m-d');
+    }*/
+
 }
---
+++ src/Clients/PatronPAPIClient.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Blashbrook\PAPIClient\Clients;
+
+use Blashbrook\PAPIClient\Concerns\Config;
+use Blashbrook\PAPIClient\Concerns\Headers;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use http\Client;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class PublicPAPIClient.
+ */
+class PatronPAPIClient extends Client
+{
+    use Headers, Config;
+
+    /**
+     * Sends public request to Polaris API.  Public requests do not
+     * require staff usernames and passwords.
+     *
+     * @param  $method  - HTTP Request method (GET|POST|PUT)
+     * @param  $requestURI
+     * @param  null[]  $params  - Optional request parameters
+     * @return ResponseInterface
+     *
+     * @throws GuzzleException
+     */
+    public static function patronRequest($method, $requestURI, array $params = [null])
+    {
+        $uri = config('papiclient.publicURI').$requestURI;
+        $headers = self::getHeaders($method, $uri);
+        $client = new Client();
+        $json = self::getPolarisSettings($params);
+
+        return $client->request($method, $uri,
+            ['headers' => $headers,
+                'json' => $json, ],
+        );
+    }
+}
